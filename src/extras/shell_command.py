79ddd4ad1d6bb8dd6ad8c8f95ddbd45ce7df57bc
felmiran <felipe.mirandar@gmail.com>
2019-08-01 16:51:15 -0400

    created function "extract_region_from_ndpi" in shell_command.py

--- src/extras/shell_command.py
+++ src/extras/shell_command.py
@@ -7,8 +7,10 @@ os.chdir("D:/felipe/ndpi")
 # este es un ejemplo de script para cortar una imagen
 # "ndpisplit -Ex40,z0,3600,13000,5000,5000,muestra_1 prueba1.ndpi"
 # generalizando, es lo que escribio juanjo (que dios le pague):
-# ndpisplit -E{magnification},{zlevel},{x},{y},{width},{height},{label} {ndpi_path}
-# ojo que el comando es sensible a espacios. Las comas tienen que ir si espacios
+# ndpisplit -E{magnification},{zlevel},{x},{y},{width},{height},{label} /
+# {ndpi_path}
+# ojo que el comando es sensible a espacios. Las comas tienen que ir sin
+# espacios
 # magnification: por ahora siempre "x40"
 # zlevel: por ahora siempre "z0"
 # x, y, width, height son en pixeles.
@@ -29,30 +31,42 @@ os.chdir("D:/felipe/ndpi")
 
 
 # Este es para agregarle un label al final a la imagen:
-# subprocess.call("ndpisplit -Ex40,z0,3600,13000,50000,5000,holi prueba1.ndpi", shell=True)
+# subprocess.call("ndpisplit -Ex40,z0,3600,13000,50000,5000,holi prueba1.ndpi",
+# shell=True)
 
 # ojo que el command es sensible a espacios!
+
+
+def extract_region_from_ndpi(magnification, zlevel, x, y, width, height, label,
+                             ndpi_path, has_parameters=True):
+
+    parameters = "ndpisplit " +  \
+            "-E{magnification},{zlevel},{x},{y},{width},{height},{label} " + \
+            "{ndpi_path}"
+
+    if has_parameters:
+        command = parameters.format(magnification=magnification,
+                                    zlevel=zlevel, x=x, y=y, width=width,
+                                    height=height, label=label,
+                                    ndpi_path=ndpi_path)
+
+    else:
+        command = ndpi_path
+
+    call(command, shell=True)
+    print("end")
+    return
+
 
 magnification = "x40"
 zlevel = "z0"
 x = 0
 y = 0
-width = 28672
+width = 15000  # original is 28672
 height = 17152
 label = "full"
 ndpi_path = "prueba2.ndpi"
 has_parameters = True
 
-if has_parameters:
-    parameters = "-E{magnification},{zlevel},{x},{y},{width},{height},{label} {ndpi_path}".format(magnification=magnification,
-                                                                                            zlevel=zlevel, x=x, y=y, width=width,
-                                                                                            height=height, label=label, 
-                                                                                            ndpi_path=ndpi_path)
-else:
-    parameters = ndpi_path
-
-command = "ndpisplit " + parameters
-
-call(command, shell=True)
-
-print("end")
+extract_region_from_ndpi(magnification, zlevel, x, y, width, height, label,
+                         ndpi_path, has_parameters=True)
